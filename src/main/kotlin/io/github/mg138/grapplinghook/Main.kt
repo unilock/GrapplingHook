5214d7bf2b4aef3da33ab7d6a50e5baafd02ae26
unilock <unilock@fennet.rentals>
2023-06-26 08:12:59 -0400

    Allow running on the logical server, too
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/kotlin/io/github/mg138/grapplinghook/Main.kt
+++ src/main/kotlin/io/github/mg138/grapplinghook/Main.kt
@@ -2,16 +2,16 @@ package io.github.mg138.grapplinghook
 
 import eu.pb4.polymer.api.resourcepack.PolymerRPUtils
 import io.github.mg138.grapplinghook.item.GrapplingHook
-import net.fabricmc.api.DedicatedServerModInitializer
+import net.fabricmc.api.ModInitializer
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 @Suppress("UNUSED")
-object Main : DedicatedServerModInitializer {
+object Main : ModInitializer {
     const val modId = "grappling_hook"
     val logger: Logger = LogManager.getLogger(modId)
 
-    override fun onInitializeServer() {
+    override fun onInitialize() {
         PolymerRPUtils.addAssetSource(modId)
 
         GrapplingHook.register()
